d3ce1c10861b0d3540a0f08cf0182c693639cf9d
Hijas <hijasajmal@gmail.com>
2017-11-20 11:24:15 +0530

    Update logindb.php

--- php_validation/logindb.php
+++ php_validation/logindb.php
@@ -2,13 +2,8 @@
 
 $tbname = "login";
 $conn = mysqli_connect("localhost", "virus", ".virusdb", "Demo")or die("cannot connect");
-//mysqli_select_db("$dbname")or die("cannot select DB");
 $myusername = $_POST['usr'];
 $mypassword = $_POST['pwd'];
-//$myusername = stripslashes($myusername);
-//$mypassword = stripslashes($mypassword);
-//$myusername = mysqli_real_escape_string($myusername);
-//$mypassword = mysqli_real_escape_string($mypassword);
 $sql = "select Name, Email,Phone from login where Name = '$myusername' AND Password = '$mypassword'";
 $result = mysqli_query($conn, $sql);
 $row = $result->fetch_assoc();
