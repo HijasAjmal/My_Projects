dfed51d538812fa2beb6bb7daa09ccfe8ce3d11f
Hijas <hijasajmal@gmail.com>
2017-11-20 11:25:20 +0530

    Update signup.php

--- php_validation/signup.php
+++ php_validation/signup.php
@@ -8,8 +8,7 @@ $phon = $_POST['phone'];
 $mypassword = $_POST['pwd'];
 $sql = "insert into login values('$myusername','$emai','$phon','$mypassword')";
 $result = mysqli_query($conn,$sql);
-$count = mysqli_num_rows($result);
-if ($count == 1)
+if ($result == 1)
 {
 	echo "Registration SUCCESS";
 	}
